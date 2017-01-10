889ff2bdacd6666273b238fff56cdaaf00e19811
Joo <joo@joostory.net>
2017-01-10 23:19:55 +0900

    code block

--- src/DeltaConverter.js
+++ src/DeltaConverter.js
@@ -1,99 +1,140 @@
 var equal = require('deep-equal')
 
 var wrapTag = function(tag, value, attrs) {
-  var attr_string = ""
-  if (attrs) {
-    for (var item in attrs) {
-      attr_string += " " + item + "='" + attrs[item] + "'"
-    }
-  }
-
-  return "<" + tag + attr_string +">" + value + "</" + tag + ">"
+    return "<" + tag + attrsToString(attrs) + ">" + value + "</" + tag + ">"
 }
-var convert = function(value, attributes) {
-  if (attributes) {
-    if (attributes['code-block']) {
-      return wrapTag("pre", wrapTag("code", value))
+
+var singleTag = function(tag, attrs) {
+    return "<" + tag + attrsToString(attrs) + ">"
+}
+
+var attrsToString = function(attrs) {
+    var attr_string = ""
+    if (attrs) {
+        for (var item in attrs) {
+            attr_string += " " + item + "='" + attrs[item] + "'"
+        }
     }
-  } else {
+    return attr_string
+}
+
+var convert = function(value, attributes) {
+    if (attributes) {
+        if (attributes['code-block']) {
+            return wrapTag("pre", wrapTag("code", value))
+        } else if (attributes['header']) {
+            return wrapTag("h" + attributes['header'], value)
+        } else if (attributes['blockquote']) {
+            return wrapTag("blockquote", value)
+        }
+    }
+
     return wrapTag("p", value)
-  }
 }
 
 var DeltaConverter = function(delta) {
-  this.results = ""
-  this.current = {
-    value:"",
-    attributes: null
-  }
-  this.delta = delta
+    this.results = ""
+    this.current = {
+        value: "",
+        attributes: null
+    }
+    this.delta = delta
 }
 
 DeltaConverter.prototype.addItem = function(insert, attributes) {
-  if (typeof insert === 'string' || insert instanceof String) {
-    if (!equal(this.current.attributes, attributes)) {
-      this.convertPrevious()
-    }
+    if (typeof insert === 'string' || insert instanceof String) {
+        if (insert.indexOf('\n') >= 0) {
+            if (equal(this.current.attributes, attributes) && attributes && attributes['code-block'] && insert == "\n") {
 
-    if (attributes) {
-      if (attributes.bold) {
-        this.current.value += wrapTag("b", insert)
-      } else if (attributes.link) {
-        this.current.value += wrapTag("a", insert, {
-          "href": attributes.link
-        })
-      } else {
+            } else {
+                this.convertPrevious()
+                this.current.attributes = attributes
+            }
+        }
+
+        if (attributes) {
+            if (attributes.bold) {
+                this.current.value += wrapTag("b", insert)
+                return
+            } else if (attributes.link) {
+                this.current.value += wrapTag("a", insert, {
+                    "href": attributes.link
+                })
+                return
+            } else if (attributes.code) {
+                this.current.value += wrapTag("code", insert)
+                return
+            }
+        }
+
         this.current.value += insert
-        this.current.attributes = attributes
-      }
-    } else {
-      this.current.value += insert
-      this.current.attributes = attributes
+        // this.current.attributes = attributes
+
+    } else if (insert.image) {
+        this.current.value += singleTag("img", {
+            "src": insert.image
+        })
     }
-  }
 }
 
 DeltaConverter.prototype.convertPrevious = function() {
-  if (this.current.value.indexOf('\n') >= 0) {
-    var values = this.current.value.split('\n'),
-      length = values.length
-    for (var i = 0 ; i < length - 1 ; i++) {
-      this.results += convert(values[i], this.current.attributes)
+    if (this.current.value.indexOf('\n') >= 0) {
+        var values = this.current.value.split('\n'),
+            length = values.length;
+
+        if (this.current.attributes && this.current.attributes['code-block']) {
+            var codeBlockValues = []
+            for (var i = 0; i < length - 1; i++) {
+                codeBlockValues.push(values[i])
+            }
+            this.results += convert(codeBlockValues.join("\n").trim(), this.current.attributes)
+        } else {
+            for (var i = 0; i < length - 1; i++) {
+                this.results += convert(values[i], this.current.attributes)
+            }
+        }
+        this.current.value = values[length - 1]
     }
-    this.current.value = values[length - 1]
-  }
 }
 
 DeltaConverter.prototype.convertRemain = function() {
-  if (this.current.value.indexOf('\n') >= 0) {
-    var values = this.current.value.split('\n'),
-      length = values.length,
-      results = ""
-    for (var i = 0 ; i < length ; i++) {
-      if (i == length - 1 && !values[i]) {
-        break
-      }
-      results += convert(values[i], this.current.attributes)
+    if (this.current.value.indexOf('\n') >= 0) {
+        var values = this.current.value.split('\n'),
+            length = values.length,
+            results = ""
+        if (this.current.attributes && this.current.attributes['code-block']) {
+            var codeBlockValues = []
+            for (var i = 0; i < length; i++) {
+                codeBlockValues.push(values[i])
+            }
+            results += convert(codeBlockValues.join("\n").trim(), this.current.attributes)
+        } else {
+            for (var i = 0; i < length; i++) {
+                if (i == length - 1 && !values[i]) {
+                    break
+                }
+                results += convert(values[i], this.current.attributes)
+            }
+        }
+        return results
+    } else {
+        return convert(this.current.value, this.current.attributes)
     }
-    return results
-  } else {
-    return convert(this.current.value, this.current.attributes)
-  }
 }
 
 DeltaConverter.prototype.toHtml = function() {
-  var ops = this.delta.ops,
-    length = ops.length
+    var ops = this.delta.ops,
+        length = ops.length
 
-  for (var i = 0 ; i < length ; i++) {
-    var insert = ops[i].insert,
-      attributes = ops[i].attributes
+    for (var i = 0; i < length; i++) {
+        var insert = ops[i].insert,
+            attributes = ops[i].attributes
 
-    this.addItem(insert, attributes)
-  }
+        this.addItem(insert, attributes)
+    }
 
-  this.results += this.convertRemain()
-  return this.results
+    this.results += this.convertRemain()
+    return this.results
 }
 
 module.exports = DeltaConverter
--- test/test.js
+++ test/test.js
@@ -1,50 +1,152 @@
 const deltaToHtml = require('../src/delta-to-html')
 
 describe('delta-to-html', () => {
-  describe('plain text', () => {
-    it('#단순 텍스트', () => {
-      expect(deltaToHtml({
-        ops:[
-          { insert:'hello,world' }
-        ]
-      })).toBe("<p>hello,world</p>")
+    describe('plain text', () => {
+        it('#단순 텍스트', () => {
+            expect(deltaToHtml({
+                ops: [{
+                    insert: 'hello,world'
+                }]
+            })).toBe("<p>hello,world</p>")
+        })
+
+        it('#여러 줄의 텍스트', () => {
+            expect(deltaToHtml({
+                ops: [{
+                    insert: 'hello\nworld\nhi\nworld'
+                }]
+            })).toBe("<p>hello</p><p>world</p><p>hi</p><p>world</p>")
+        })
+
+        it('#bold', () => {
+            expect(deltaToHtml({
+                ops: [{
+                        insert: 'Hello'
+                    },
+                    {
+                        insert: 'World',
+                        attributes: {
+                            bold: true
+                        }
+                    }
+                ]
+            })).toBe("<p>Hello<b>World</b></p>")
+        })
+
+        it('#link', () => {
+            expect(deltaToHtml({
+                ops: [{
+                        insert: "Go to "
+                    },
+                    {
+                        insert: "joostory",
+                        attributes: {
+                            link: "https://joostory.github.io"
+                        }
+                    }
+                ]
+            })).toBe("<p>Go to <a href='https://joostory.github.io'>joostory</a></p>")
+        })
+
+        it('#code', () => {
+            expect(deltaToHtml({
+                ops: [{
+                        insert: 'Hello'
+                    },
+                    {
+                        insert: 'World',
+                        attributes: {
+                            code: true
+                        }
+                    }
+                ]
+            })).toBe("<p>Hello<code>World</code></p>")
+        })
     })
 
-    it('#여러 줄의 텍스트', () => {
-      expect(deltaToHtml({
-        ops: [
-          { insert:'hello\nworld\nhi\nworld' }
-        ]
-      })).toBe("<p>hello</p><p>world</p><p>hi</p><p>world</p>")
+    describe('block attributes', () => {
+        it('#code-block', () => {
+            expect(deltaToHtml({
+                ops: [{
+                        insert: "Say 'hello, world!'\nconsole.log('hello, world!')"
+                    },
+                    {
+                        insert: '\n',
+                        attributes: {
+                            'code-block': true
+                        }
+                    }
+                ]
+            })).toBe("<p>Say 'hello, world!'</p><pre><code>console.log('hello, world!')</code></pre>")
+        })
+
+        it('#code-block multiline', () => {
+            expect(deltaToHtml({
+                ops: [{
+                        insert: "Say 'hello, world!'\nconsole.log('hello, world!')"
+                    },
+                    {
+                        insert: '\n',
+                        attributes: {
+                            'code-block': true
+                        }
+                    },
+                    {
+                        insert: "console.log('hi, world!')"
+                    },
+                    {
+                        insert: '\n',
+                        attributes: {
+                            'code-block': true
+                        }
+                    },
+                    {
+                        insert: "done\n"
+                    }
+                ]
+            })).toBe("<p>Say 'hello, world!'</p><pre><code>console.log('hello, world!')\nconsole.log('hi, world!')</code></pre><p>done</p>")
+        })
+
+        it('#header1', () => {
+            expect(deltaToHtml({
+                ops: [{
+                        insert: "Say 'hello, world!'\nconsole.log('hello, world!')"
+                    },
+                    {
+                        insert: '\n',
+                        attributes: {
+                            'header': 1
+                        }
+                    }
+                ]
+            })).toBe("<p>Say 'hello, world!'</p><h1>console.log('hello, world!')</h1>")
+        })
+
+        it('#blockquote', () => {
+            expect(deltaToHtml({
+                ops: [{
+                        insert: "Say 'hello, world!'\nconsole.log('hello, world!')"
+                    },
+                    {
+                        insert: '\n',
+                        attributes: {
+                            'blockquote': true
+                        }
+                    }
+                ]
+            })).toBe("<p>Say 'hello, world!'</p><blockquote>console.log('hello, world!')</blockquote>")
+        })
     })
 
-    it('#bold', () => {
-      expect(deltaToHtml({
-        ops: [
-          { insert: 'Hello' },
-          { insert: 'World', attributes:{ bold:true} }
-        ]
-      })).toBe("<p>Hello<b>World</b></p>")
+    describe('media', () => {
+        it('#image', () => {
+            expect(deltaToHtml({
+                ops: [{
+                    insert: {
+                        image: "http://test.com/image.png"
+                    }
+                }]
+            })).toBe("<p><img src='http://test.com/image.png'></p>")
+        })
     })
-
-    it('#link', () => {
-      expect(deltaToHtml({
-        ops: [
-          { insert: "Go to " },
-          { insert: "joostory", attributes: { link:"https://joostory.github.io" } }
-        ]
-      })).toBe("<p>Go to <a href='https://joostory.github.io'>joostory</a></p>")
-    })
-  })
-
-  describe('block attributes', () => {
-    it('#code-block', () => {
-      expect(deltaToHtml({
-        ops: [
-          { insert: "Say 'hello, world!'\nconsole.log('hello, world!')" },
-          { insert: '\n', attributes: {'code-block': true}}
-        ]
-      })).toBe("<p>Say 'hello, world!'</p><pre><code>console.log('hello, world!')</code></pre>")
-    })
-  })
 })
